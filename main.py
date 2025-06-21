ab62d0046cf868dd48555720ab5e4f9fbd4fd55c
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-21 14:17:33 +0700

    float in query

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q: Annotated[str | None, Query(alias="item-query")] = None,
+    *,
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    q: str,
+    size: Annotated[float, Query(gt=0, lt=10.5)],
 ):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
-
+    if size:
+        results.update({"size": size})
     return results

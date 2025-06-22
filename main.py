078790ae09025fa8ce475644258f41b540b6a166
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-22 09:16:04 +0700

    add example schema

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
+class Item(BaseModel):
     name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
 
 
-@app.post("/images/multiple/")
-async def create_multiple_images(images: list[Image]):
-    return images
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results

32bfd97d82e9579a1a6ea2ae18936c96e75dbb20
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-22 22:55:34 +0700

    Duplicate headers

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(user_agent: Annotated[str | None, Header()] = None):
+async def read_items(user_agent: Annotated[list[str] | None, Header()] = None):
     return {"User-Agent": user_agent}

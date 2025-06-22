aa74a6ea03c2d218fcd8a8400d2e0a0858b096f3
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-22 23:40:06 +0700

    Header Parameters with a Pydantic Model

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class CommonHeaders(BaseModel):
+    host: str
+    save_data: bool
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = []
+
+
 @app.get("/items/")
-async def read_items(user_agent: Annotated[list[str] | None, Header()] = None):
-    return {"User-Agent": user_agent}
+async def read_items(headers: Annotated[CommonHeaders, Header()]):
+    return headers

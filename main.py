40879c6000feceec380305a8c6035ff78c2ca48b
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-23 09:57:14 +0700

    Return a Response Directly

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-from typing import Any
-
-from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse, RedirectResponse
 
 app = FastAPI()
 
 
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: str | None = None
-
-
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
-
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn) -> Any:
-    return user
+@app.get("/portal")
+async def get_portal(teleport: bool = False) -> Response:
+    if teleport:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return JSONResponse(content={"message": "Here's your interdimensional portal."})

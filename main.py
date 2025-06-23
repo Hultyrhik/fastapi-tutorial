46c4c3b0daf3a2617255b8c5ac56abc6be4f8312
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-23 11:34:36 +0700

    return list of models

--- main.py
+++ main.py
@@ -1,35 +1,20 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class BaseItem(BaseModel):
+class Item(BaseModel):
+    name: str
     description: str
-    type: str
 
 
-class CarItem(BaseItem):
-    type: str = "car"
+items = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
 
 
-class PlaneItem(BaseItem):
-    type: str = "plane"
-    size: int
-
-
-items = {
-    "item1": {"description": "All my friends drive a low rider", "type": "car"},
-    "item2": {
-        "description": "Music is my aeroplane, it's my aeroplane",
-        "type": "plane",
-        "size": 5,
-    },
-}
-
-
-@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
-async def read_item(item_id: str):
-    return items[item_id]
+@app.get("/items/", response_model=list[Item])
+async def read_items():
+    return items

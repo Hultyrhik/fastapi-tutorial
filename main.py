4ca8dd01b93d848f1d4b3686cc070cb69536c351
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-24 10:09:29 +0700

    Classes as dependencies

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from fastapi import Depends, FastAPI
 app = FastAPI()
 
 
-async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-CommonsDep = Annotated[dict, Depends(common_parameters)]
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
 
 @app.get("/items/")
-async def read_items(commons: CommonsDep):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: CommonsDep):
-    return commons
+async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response

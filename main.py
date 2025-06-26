2ad90399d483127a60137ef39458de672925816e
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-26 23:02:35 +0700

    close session

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Session
 
 
 class Hero(SQLModel, table=True):
@@ -17,6 +17,23 @@ engine = create_engine(sqlite_url, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+def create_heroes():
+    hero_1 = Hero(name="Dead", secret_name="Dive")
+    hero_2 = Hero(name="Spider", secret_name="Piter")
+    hero_3 = Hero(name="Iron", secret_name="Tommy", age=48)
+
+    session = Session(engine)
+
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+    session.commit()
+
+    session.close()
+
+def main():
+    create_db_and_tables()
+    create_heroes()
 
 if __name__ == "__main__":
-    create_db_and_tables()
+    main()

e9b3df2a1865f7b2c4460aecdc6e65632a4b5385
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-26 23:52:53 +0700

    add .where() With Multiple Expressions

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def create_heroes():
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero).where(Hero.age > 35)
+        statement = select(Hero).where(Hero.age >= 35, Hero.age < 40)
         results = session.exec(statement)
         for hero in results:
             print(hero)

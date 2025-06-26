849e931c047a9d59ce41bb1c528471a60f763c33
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-26 23:42:51 +0700

    return select all in list

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def select_heroes():
     with Session(engine) as session:  
         statement = select(Hero)  
         results = session.exec(statement)  
-        for hero in results:  
-            print(hero)  
+        heroes = results.all()
+        print(heroes)
     
 
 

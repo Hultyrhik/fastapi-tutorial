d0b60990f89968afa97ec43ce23a8cb6784e1940
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-26 23:03:16 +0700

    close session using with keyword

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 
 class Hero(SQLModel, table=True):
@@ -17,23 +17,25 @@ engine = create_engine(sqlite_url, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def create_heroes():
-    hero_1 = Hero(name="Dead", secret_name="Dive")
-    hero_2 = Hero(name="Spider", secret_name="Piter")
-    hero_3 = Hero(name="Iron", secret_name="Tommy", age=48)
 
-    session = Session(engine)
+def create_heroes():  
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")  
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
 
-    session.add(hero_1)
-    session.add(hero_2)
-    session.add(hero_3)
-    session.commit()
+    with Session(engine) as session:  
+        session.add(hero_1)  
+        session.add(hero_2)
+        session.add(hero_3)
 
-    session.close()
+        session.commit()  
+    
 
-def main():
-    create_db_and_tables()
-    create_heroes()
 
-if __name__ == "__main__":
+def main():  
+    create_db_and_tables()  
+    create_heroes()  
+
+
+if __name__ == "__main__":  
     main()

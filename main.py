f8b1ae729b6323bfbdbb1928f7f31a0d228ab37d
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-27 19:13:31 +0700

    add indexes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from sqlmodel import or_, col, Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(index=True)
     secret_name: str
-    age: int | None = None
+    age: int | None = Field(default=None, index=True)
 
 
 sqlite_file_name = "database.db"
@@ -22,26 +22,18 @@ def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
-    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
-    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
-    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
-        session.add(hero_4)
-        session.add(hero_5)
-        session.add(hero_6)
-        session.add(hero_7)
 
         session.commit()
 
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero).where(or_(col(Hero.age) >= "zcxc", col(Hero.age) < 40))
+        statement = select(Hero).where(Hero.name == "Deadpond")
         results = session.exec(statement)
         for hero in results:
             print(hero)

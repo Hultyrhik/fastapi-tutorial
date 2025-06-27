2cd87651cff67d6bb361427f03abac7f6a96b4af
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-06-27 19:42:04 +0700

    add select only one

--- main.py
+++ main.py
@@ -22,11 +22,19 @@ def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
@@ -35,7 +43,7 @@ def select_heroes():
     with Session(engine) as session:
         statement = select(Hero).where(Hero.name == "Deadpond")
         results = session.exec(statement)
-        hero = results.first()
+        hero = results.one()
         print("Hero:", hero)
 
 

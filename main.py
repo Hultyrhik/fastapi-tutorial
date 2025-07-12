61efab32b15a775a815dfe198037abb1e9e23bb4
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-07-12 19:36:36 +0700

    add create data function and implement pagination logic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, func, col
+
+import string
+import random
 
 
 class HeroBase(SQLModel):
@@ -30,6 +33,19 @@ engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+def id_generator(size: int=6, chars:str=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def create_data():
+    with Session(engine) as session:
+        for _ in range(50):
+            model_db = Hero(
+                name=id_generator(),
+                secret_name=id_generator(),
+                age=random.randint(0,100)
+            )
+            session.add(model_db)
+            session.commit()
 
 app = FastAPI()
 
@@ -37,6 +53,8 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+    create_data()
 
 
 @app.post("/heroes/", response_model=HeroPublic)
@@ -49,11 +67,28 @@ def create_hero(hero: HeroCreate):
         return db_hero
 
 
-@app.get("/heroes/", response_model=list[HeroPublic])
-def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
+@app.get(
+        "/heroes/",
+        response_model=list[HeroPublic]
+        )
+def read_heroes(page: int = Query(default=1, ge=1), per_page: int = Query(alias="per-page", default=100, le=100, ge=0)):
     with Session(engine) as session:
-        heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+        count_statement = select(func.count()).select_from(Hero).where(col(Hero.age) > 0)
+        total = session.exec(count_statement).one()
+        
+        pages = (total // per_page) + 1
+        page = page - 1
+        offset = per_page * page
+        
+        heroes = session.exec(select(Hero).offset(offset).limit(per_page)).all()
         return heroes
+        return {
+            "page": page,
+            "per_page": per_page,
+            "total": total,
+            "pages": pages,
+        }
+
 
 
 @app.get("/heroes/{hero_id}", response_model=HeroPublic)

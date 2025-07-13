927a9c38769addf05a74ff82426f874566fdcc42
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-07-13 11:20:41 +0700

    Update Data with FastAPI

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select, func, col
-
-import string
-import random
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class HeroBase(SQLModel):
@@ -23,6 +20,12 @@ class HeroPublic(HeroBase):
     id: int
 
 
+class HeroUpdate(SQLModel):
+    name: str | None = None
+    secret_name: str | None = None
+    age: int | None = None
+
+
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
@@ -33,19 +36,6 @@ engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-def id_generator(size: int=6, chars:str=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-def create_data():
-    with Session(engine) as session:
-        for _ in range(50):
-            model_db = Hero(
-                name=id_generator(),
-                secret_name=id_generator(),
-                age=random.randint(0,100)
-            )
-            session.add(model_db)
-            session.commit()
 
 app = FastAPI()
 
@@ -53,8 +43,6 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-
-    create_data()
 
 
 @app.post("/heroes/", response_model=HeroPublic)
@@ -67,28 +55,11 @@ def create_hero(hero: HeroCreate):
         return db_hero
 
 
-@app.get(
-        "/heroes/",
-        response_model=list[HeroPublic]
-        )
-def read_heroes(page: int = Query(default=1, ge=1), per_page: int = Query(alias="per-page", default=100, le=100, ge=0)):
+@app.get("/heroes/", response_model=list[HeroPublic])
+def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
     with Session(engine) as session:
-        count_statement = select(func.count()).select_from(Hero).where(col(Hero.age) > 0)
-        total = session.exec(count_statement).one()
-        
-        pages = (total // per_page) + 1
-        page = page - 1
-        offset = per_page * page
-        
-        heroes = session.exec(select(Hero).offset(offset).limit(per_page)).all()
+        heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
         return heroes
-        return {
-            "page": page,
-            "per_page": per_page,
-            "total": total,
-            "pages": pages,
-        }
-
 
 
 @app.get("/heroes/{hero_id}", response_model=HeroPublic)
@@ -98,3 +69,17 @@ def read_hero(hero_id: int):
         if not hero:
             raise HTTPException(status_code=404, detail="Hero not found")
         return hero
+
+
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate):
+    with Session(engine) as session:
+        db_hero = session.get(Hero, hero_id)
+        if not db_hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        hero_data = hero.model_dump(exclude_unset=True)
+        db_hero.sqlmodel_update(hero_data)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+        return db_hero

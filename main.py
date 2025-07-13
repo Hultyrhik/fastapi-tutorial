a482b5bfb6cb150c7c1d09e8f9580850fcc5213e
Fyodor Bashlakov <feiaoduoit@gmail.com>
2025-07-14 00:41:08 +0700

    Delete Data with FastAPI

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ class HeroBase(SQLModel):
 
 class Hero(HeroBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    hashed_password: str = Field()
 
 
 class HeroCreate(HeroBase):
-    password: str
+    pass
 
 
 class HeroPublic(HeroBase):
@@ -25,7 +24,6 @@ class HeroUpdate(SQLModel):
     name: str | None = None
     secret_name: str | None = None
     age: int | None = None
-    password: str | None = None
 
 
 sqlite_file_name = "database.db"
@@ -39,11 +37,6 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def hash_password(password: str) -> str:
-    # Use something like passlib here
-    return f"not really hashed {password} hehehe"
-
-
 app = FastAPI()
 
 
@@ -54,10 +47,8 @@ def on_startup():
 
 @app.post("/heroes/", response_model=HeroPublic)
 def create_hero(hero: HeroCreate):
-    hashed_password = hash_password(hero.password)
     with Session(engine) as session:
-        extra_data = {"hashed_password": hashed_password}
-        db_hero = Hero.model_validate(hero, update=extra_data)
+        db_hero = Hero.model_validate(hero)
         session.add(db_hero)
         session.commit()
         session.refresh(db_hero)
@@ -87,13 +78,19 @@ def update_hero(hero_id: int, hero: HeroUpdate):
         if not db_hero:
             raise HTTPException(status_code=404, detail="Hero not found")
         hero_data = hero.model_dump(exclude_unset=True)
-        extra_data = {}
-        if "password" in hero_data:
-            password = hero_data["password"]
-            hashed_password = hash_password(password)
-            extra_data["hashed_password"] = hashed_password
-        db_hero.sqlmodel_update(hero_data, update=extra_data)
+        db_hero.sqlmodel_update(hero_data)
         session.add(db_hero)
         session.commit()
         session.refresh(db_hero)
         return db_hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        session.delete(hero)
+        session.commit()
+        return {"ok": True}
